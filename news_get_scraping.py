03cfdd7c731e1b967864429ed3a3360abb630116
morimoro <kyuzaemon.miza@gmail.com>
2022-12-28 13:30:15 +0900

    221228途中

--- news_get_scraping.py
+++ news_get_scraping.py
@@ -9,12 +9,18 @@ soup = BeautifulSoup(r.text, "html.parser")
 # divタグのクラスを指定して、ニュース一つ分のURLを取得
 elems = soup.find("div", class_ = "p-index__single p-index__news")
 elems_news = elems.find_all("a")
-url_news = elems_news[1].get("href")
+url_news_list = []
+for elem_news in elems_news:
+    url_news_list.append(elem_news.get("href"))
 
-r_2 = requests.get(url_news)
+r_2 = requests.get(url_news_list[0])
 soup_2 = BeautifulSoup(r_2.text, "html.parser")
 
-elems_2 = soup_2.find("div", class_ = "l-content__main")
+elems_2 = soup_2.find("div", class_ = "p-post__body")
 elems_news_2 = elems_2.find_all("p")
 
-print(elems_news_2)
+newslist = ""
+for elem_news_2 in elems_news_2:
+    newslist += elem_news_2.get_text(strip=True)
+
+print(newslist)
