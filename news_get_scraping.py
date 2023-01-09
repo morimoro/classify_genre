e2502305692bdafe7df78fb1513cc791476acc6c
Takuya <kyuzaemon.miza@gmail.com>
2023-01-09 22:17:50 +0900

    news_text_list を出力まで完成

--- news_get_scraping.py
+++ news_get_scraping.py
@@ -6,21 +6,34 @@ url = "https://www.kankyo-business.jp/solar/news"
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
 
-# divタグのクラスを指定して、ニュース一つ分のURLを取得
+# divタグのクラスを指定して、ニュースそれぞれのURLを取得し、リスト化
 elems = soup.find("div", class_ = "p-index__single p-index__news")
 elems_news = elems.find_all("a")
 url_news_list = []
 for elem_news in elems_news:
     url_news_list.append(elem_news.get("href"))
 
-r_2 = requests.get(url_news_list[0])
-soup_2 = BeautifulSoup(r_2.text, "html.parser")
+# ニュースそれぞれに介してニュース本文を取得し、news_text_listに追加
+news_text_list = []
+for i in range(len(url_news_list)):
+    r_2 = requests.get(url_news_list[i])
+    soup_2 = BeautifulSoup(r_2.text, "html.parser")
+    elems_2 = soup_2.find("div", class_ = "p-post__body")
+    elems_news_2 = elems_2.find_all("p")
+    news_text = ""
+    for elem_news_2 in elems_news_2:
+        news_text += elem_news_2.get_text(strip=True)
+    news_text_list.append(news_text)
 
-elems_2 = soup_2.find("div", class_ = "p-post__body")
-elems_news_2 = elems_2.find_all("p")
+print(news_text_list)
 
-newslist = ""
-for elem_news_2 in elems_news_2:
-    newslist += elem_news_2.get_text(strip=True)
+import csv
 
-print(newslist)
+csv_path = "test.csv"
+
+with open(csv_path, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["No.", "Title", "Text"])
+    writer.writerow([1, "Apple", 100])
+    writer.writerow([2, "Banana", 200])
+    writer.writerow([3, "Orange", 100])
